78fd8ebccce5a81cea45c0c923fb73a4d14c1c56
izocuna <izocuna@gmail.com>
2023-07-01 05:59:13 +0900

    add

--- src/main/java/com/four/letmego/controller/DemoController.java
+++ src/main/java/com/four/letmego/controller/DemoController.java
@@ -1,12 +1,15 @@
-package controller;
+package com.four.letmego.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+  @Value("${override.server}")
+  String value;
   @GetMapping("/demo")
   public String demo() {
-    return "demo";
+    return "demo " + value;
   }
 }
