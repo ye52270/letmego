97b4923769ebf24783835ffc38db9251392450e2
izocuna <izocuna@gmail.com>
2023-07-01 01:44:38 +0900

    first commit

---
+++ src/main/java/com/four/letmego/LetmegoApplication.java
@@ -0,0 +1,13 @@
+package com.four.letmego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmegoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LetmegoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/four/letmego/LetmegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.four.letmego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetmegoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
