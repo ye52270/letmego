88aadc11751aeae95d02ce172602fbd36df00e62
izocuna <izocuna@gmail.com>
2023-07-01 03:33:19 +0900

    sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROJECT_NAME="letmego"
+PROJECT_NAME="github_action"
 JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
